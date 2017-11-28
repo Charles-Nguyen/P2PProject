5af61f5cb8dfbc43fc883e8933a727d2c6f7b2e2
sbthegreat <connorbl93@yahoo.com>
2017-11-28 15:51:51 -0800

    Adding main program entry point
    PortBroadcast is where the program starts running from, and it calls
    server or client depending on which role the user is currently taking
    on.

--- FileTransferClient.java
+++ FileTransferClient.java
@@ -13,149 +13,146 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+public class FileTransferClient {
+	public static void connect(String ip) throws Exception {
+		// Initialize socket
+		System.out.println("This is the client running");
+		Socket socket = new Socket(InetAddress.getByName(ip), 5000);
+		byte[] contents = new byte[10000];
+		//System.out.println(socket.getLocalPort());
 
-public class FileTransferClient
-{
-	public static void main(String[] args) throws Exception
-    {   
-        //Initialize socket
-        Socket socket = new Socket(InetAddress.getByName("localhost"), 5000);
-        byte[] contents = new byte[10000];
-        
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        InputStream is = socket.getInputStream();
-        //No of bytes read in one read() call
-        int bytesRead = 0;
-        
-        while ((bytesRead = is.read(contents)) != -1)
-            stream.write(contents, 0, bytesRead);
-        
-        System.out.println(stream);
-        String[] serverFiles = stream.toString().split("/");
-        File root = new File("clientFiles");
-        File[] toSend = root.listFiles();
-        ArrayList<String> clientFiles = new ArrayList<String>();
-        for (File f : toSend)
-        	clientFiles.add(f.getName());
-        
-        String neededFiles = "";
-        for (String n : serverFiles)
-        {
-        	if (!clientFiles.contains(n))
-        	{
-        		neededFiles += n + "/";
-        	}
-        }
-        
-        ArrayList<String> serverFiles2 = new ArrayList<>(Arrays.asList(serverFiles));
-        neededFiles += "|";
-        for (String n : clientFiles)
-        {
-        	if (!serverFiles2.contains(n))
-        	{
-        		neededFiles += n + "/";
-        	}
-        }
-        System.out.println(neededFiles);
-        String[] serverNeedsFiles = (neededFiles.split("\\|")[1]).split("/");
-        
-        stream.close();
-        is.close();
-        socket.close();
-        
-        socket = new Socket(InetAddress.getByName("localhost"), 5000);
-        
-        // START SENDING BACK LIST OF FILES CLIENT NEEDS
-        InputStream sendBack = new ByteArrayInputStream(neededFiles.getBytes(StandardCharsets.UTF_8.name()));
-        BufferedInputStream bis = new BufferedInputStream(sendBack);
-        
-        OutputStream os = socket.getOutputStream();
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		InputStream is = socket.getInputStream();
+		// No of bytes read in one read() call
+		int bytesRead = 0;
 
-        long fileLength = sendBack.available();
-        long current = 0;
+		while ((bytesRead = is.read(contents)) != -1)
+			stream.write(contents, 0, bytesRead);
 
-        while (current != fileLength)
-        {
-            int size = 10000;
-            if (fileLength - current >= size)
-                current += size;
-            else
-            {
-                size = (int)(fileLength - current); 
-                current = fileLength;
-            }
-            contents = new byte[size];
-            bis.read(contents, 0, size);
-            os.write(contents);
-            System.out.print("Sending file ... " + (current * 100) / fileLength + "% complete! ");
-        }
-        
-        os.flush();
-        bis.close();
-        socket.close();
-        
-        // END
-        
-        // SENDING FILES THAT SERVER NEEDS
-        for (String shop : serverNeedsFiles)
-        {
-        	socket = new Socket(InetAddress.getByName("localhost"), 5000);
-            contents = new byte[10000];
-        	File file = new File("clientFiles\\" + shop);
-            FileInputStream fis = new FileInputStream(file);
-            bis = new BufferedInputStream(fis); 
-            
-            os = socket.getOutputStream();
-            
-            fileLength = file.length(); 
-            current = 0;
-            
-            while (current != fileLength)
-            { 
-                int size = 10000;
-                if (fileLength - current >= size)
-                    current += size;    
-                else
-                { 
-                    size = (int)(fileLength - current); 
-                    current = fileLength;
-                } 
-                contents = new byte[size]; 
-                bis.read(contents, 0, size); 
-                os.write(contents);
-                System.out.print("Sending file ... "+(current*100)/fileLength+"% complete!");
-            }
-            os.flush();
-            bis.close();
-            socket.close();
-        }
-        
-        //START RECEIVING FILES NEEDED FROM SERVER
-        String[] receivedFiles = (neededFiles.split("\\|")[0]).split("/");
-        System.out.println(receivedFiles.length);
-        for (String slap : receivedFiles)
-        {
-        	 System.out.println(slap);
-        	 socket = new Socket(InetAddress.getByName("localhost"), 5000);
-             contents = new byte[10000];
-             
-             //Initialize the FileOutputStream to the output file's full path.
-             FileOutputStream fos = new FileOutputStream("clientFiles\\" + slap);
-             BufferedOutputStream bos = new BufferedOutputStream(fos);
-             is = socket.getInputStream();
-             
-             //No of bytes read in one read() call
-             bytesRead = 0; 
-             
-             while((bytesRead = is.read(contents)) != -1)
-                 bos.write(contents, 0, bytesRead); 
-             
-             bos.flush();
-             bos.close();
-             is.close();
-             socket.close(); 
-        }
-        
-        System.out.println("File saved successfully!");
-    }
+		//System.out.println(stream);
+		String[] serverFiles = stream.toString().split("/");
+		File root = new File(
+				"C:\\Users\\Connor\\workspace\\CECS327\\src\\Files");
+		File[] toSend = root.listFiles();
+		ArrayList<String> Files = new ArrayList<String>();
+		for (File f : toSend)
+			Files.add(f.getName());
+
+		String neededFiles = "";
+		for (String n : serverFiles) {
+			if (!Files.contains(n)) {
+				neededFiles += n + "/";
+			}
+		}
+
+		ArrayList<String> serverFiles2 = new ArrayList<>(
+				Arrays.asList(serverFiles));
+		neededFiles += "|";
+		for (String n : Files) {
+			if (!serverFiles2.contains(n)) {
+				neededFiles += n + "/";
+			}
+		}
+		//System.out.println(neededFiles);
+		String[] serverNeedsFiles = (neededFiles.split("\\|")[1]).split("/");
+
+		stream.close();
+		is.close();
+		socket.close();
+
+		socket = new Socket(InetAddress.getByName(ip), 5000);
+
+		// START SENDING BACK LIST OF FILES CLIENT NEEDS
+		InputStream sendBack = new ByteArrayInputStream(
+				neededFiles.getBytes(StandardCharsets.UTF_8.name()));
+		BufferedInputStream bis = new BufferedInputStream(sendBack);
+
+		OutputStream os = socket.getOutputStream();
+
+		long fileLength = sendBack.available();
+		long current = 0;
+
+		while (current != fileLength) {
+			int size = 10000;
+			if (fileLength - current >= size)
+				current += size;
+			else {
+				size = (int) (fileLength - current);
+				current = fileLength;
+			}
+			contents = new byte[size];
+			bis.read(contents, 0, size);
+			os.write(contents);
+			//System.out.print("Sending file ... " + (current * 100) / fileLength + "% complete! ");
+		}
+
+		os.flush();
+		bis.close();
+		socket.close();
+
+		// END
+
+		// SENDING FILES THAT SERVER NEEDS
+		for (String shop : serverNeedsFiles) {
+			socket = new Socket(InetAddress.getByName(ip), 5000);
+			contents = new byte[10000];
+			File file = new File(
+					"C:\\Users\\Connor\\workspace\\CECS327\\src\\Files\\"
+							+ shop);
+			FileInputStream fis = new FileInputStream(file);
+			bis = new BufferedInputStream(fis);
+
+			os = socket.getOutputStream();
+
+			fileLength = file.length();
+			current = 0;
+
+			while (current != fileLength) {
+				int size = 10000;
+				if (fileLength - current >= size)
+					current += size;
+				else {
+					size = (int) (fileLength - current);
+					current = fileLength;
+				}
+				contents = new byte[size];
+				bis.read(contents, 0, size);
+				os.write(contents);
+				//System.out.print("Sending file ... " + (current * 100) / fileLength + "% complete!");
+			}
+			os.flush();
+			bis.close();
+			socket.close();
+		}
+
+		// START RECEIVING FILES NEEDED FROM SERVER
+		String[] receivedFiles = (neededFiles.split("\\|")[0]).split("/");
+		//System.out.println(receivedFiles.length);
+		for (String slap : receivedFiles) {
+			//System.out.println(slap);
+			socket = new Socket(InetAddress.getByName(ip), 5000);
+			contents = new byte[10000];
+
+			// Initialize the FileOutputStream to the output file's full path.
+			FileOutputStream fos = new FileOutputStream(
+					"C:\\Users\\Connor\\workspace\\CECS327\\src\\Files\\"
+							+ slap);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			is = socket.getInputStream();
+
+			// No of bytes read in one read() call
+			bytesRead = 0;
+
+			while ((bytesRead = is.read(contents)) != -1)
+				bos.write(contents, 0, bytesRead);
+
+			bos.flush();
+			bos.close();
+			is.close();
+			socket.close();
+		}
+
+		System.out.println("File saved successfully!");
+	}
+
 }
---
+++ PortBroadcast.java
@@ -0,0 +1,71 @@
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class PortBroadcast implements Runnable {
+	public static HashSet<String> ipsSynched = new HashSet<String>();
+	public static void main(String[] args) throws Exception {
+		Thread clientThread = new Thread(new PortBroadcast());
+		clientThread.start();
+		while (true) {
+			ServerSocket ssock = new ServerSocket(4999);
+			Socket sock = ssock.accept();
+			//System.out.println(sock.getInetAddress());
+			FileTransferServer.connect();
+			System.out.println("ip server saves: " + sock.getInetAddress().toString());
+			ipsSynched.add(sock.getInetAddress().toString());
+			sock.close();
+			ssock.close();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+
+			try {
+				ArrayList<String> ipsToReach = getIps();
+				for (String ip : ipsToReach) {
+					System.out.println(ipsSynched);
+					if (!ipsSynched.contains("/" + ip))
+					{
+						Socket sock = new Socket(InetAddress.getByName(ip), 4999);
+						FileTransferClient.connect(ip);
+						System.out.println("ip client saves: " + sock.getInetAddress().toString());
+						ipsSynched.add(sock.getInetAddress().toString());
+						sock.close();
+					}
+				}
+			} catch (ConnectException e) {
+				System.out.println("Timed out");
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	public static ArrayList<String> getIps() throws Exception {
+		Scanner s = new Scanner(Runtime.getRuntime().exec("arp -a")
+				.getInputStream());
+		boolean relevantIpsLeft = true;
+		ArrayList<String> ipsToReach = new ArrayList<String>();
+		while (s.hasNextLine()/* relevantIpsLeft */) {
+			String currentLine = s.nextLine();
+			// System.out.println(currentLine);
+			if (!currentLine.equals("")) {
+				Scanner checkLine = new Scanner(currentLine);
+				String ip = checkLine.next();
+				if (ip.contains("192.168.43") && !ip.equals("192.168.43.1")
+						&& !ip.equals("192.168.43.255")) {
+					ipsToReach.add(ip);
+				}
+				checkLine.close();
+			}
+		}
+		s.close();
+		return ipsToReach;
+	}
+}
